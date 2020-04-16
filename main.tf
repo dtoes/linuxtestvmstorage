1cd7cf5c9fb5a18ec33d4d9d484e8d255167be80
Daan Toes <daan.toes@avanade.com>
2020-04-16 15:25:52 +0200

    op F4 blijven, alleen webapp container toevoegen

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "azurerm_virtual_machine" "example" {
   location              = azurerm_resource_group.main.location
   resource_group_name   = azurerm_resource_group.main.name
   network_interface_ids = [azurerm_network_interface.linux.id]
-  vm_size               = "Standard_F2"
+  vm_size               = "Standard_F4"
 
   storage_image_reference {
     publisher = "Canonical"
